3972522ea3003122b8a73f6a2fa83a2c0a902dbb
Dallas G <32652704+DallasGianniny@users.noreply.github.com>
2017-11-16 14:06:14 -0500

    Add files via upload

---
+++ arduino code/first_stepper_motor_code/first_stepper_motor_code.ino
@@ -0,0 +1,42 @@
+#include <Stepper.h>
+
+//int PUL=11; //define Pulse pin
+//int DIR=10; //define Direction pin
+
+#define STEPS 200
+
+Stepper stepMotor1(STEPS, 10, 11);
+
+void setup() {
+//  pinMode (PUL, OUTPUT);
+//  pinMode (DIR, OUTPUT);
+ stepMotor1.setSpeed(1900);
+}
+
+void loop() {
+  stepMotor1.step(800);
+  delay(60);
+  stepMotor1.step(-800);
+  delay(60);
+  
+ /* for (int i=0; i<800; i++)    //Forward 5000 steps
+  {
+    digitalWrite(DIR,LOW);
+    digitalWrite(PUL,HIGH);
+    delayMicroseconds(21);
+    digitalWrite(PUL,LOW);
+    delayMicroseconds(21);
+  }
+  delay(250);
+  /*
+  for (int i=0; i<600; i++)   //Backward 5000 steps
+  {
+    digitalWrite(DIR,HIGH);
+    
+    digitalWrite(PUL,HIGH);
+    delayMicroseconds(50);
+    digitalWrite(PUL,LOW);
+    delayMicroseconds(50);
+  } 
+  */
+}
